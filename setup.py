f4cdb85ec068e684030d466f32a7e7076121bf9f
Łukasz Piekarski <wookashp98@gmail.com>
2023-02-02 18:25:38 +0100

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="intruder",
-    version="0.1.1",
+    version="0.1.2",
 
     url='https://github.com/lpiekarski/intruder',
     author='Łukasz Piekarski',
